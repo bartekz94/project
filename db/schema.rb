984199b725cfa85169dede333685de6e2b43719a
bartekz94 <b.zaucha1@nuigalway.ie>
2016-11-04 20:46:30 +0000

    added cart and line item

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161010152140) do
+ActiveRecord::Schema.define(version: 20161104204201) do
 
   create_table "apps", force: true do |t|
     t.string   "name"
@@ -23,5 +23,20 @@ ActiveRecord::Schema.define(version: 20161010152140) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "carts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "line_items", force: true do |t|
+    t.integer  "product_id"
+    t.integer  "cart_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "line_items", ["cart_id"], name: "index_line_items_on_cart_id"
+  add_index "line_items", ["product_id"], name: "index_line_items_on_product_id"
 
 end
