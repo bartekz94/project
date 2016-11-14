b342b6cae1dd4d3114af3d78ddc691eca139ae61
bartekz94 <b.zaucha1@nuigalway.ie>
2016-11-14 14:09:12 +0000

    added quantity column to line items table

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,15 +1,15 @@
 class Cart < ActiveRecord::Base
 has_many :line_items, dependent: :destroy
 
-  def add_product(product_id)
-    current_item = line_items.find_by(product_id: product_id)
-    if current_item
-      current_item.quantity += 1
-    else
-      current_item = line_items.build(product_id: product_id)
-    end
-    current_item
+  def add_app(app_id)
+  current_item = line_items.find_by(app_id: app_id)
+  if current_item
+    current_item.quantity += 1
+  else
+    current_item = line_items.build(app_id: app_id)
   end
+  current_item
+end
   def total_price
     line_items.to_a.sum { |item| item.total_price }
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161104204201) do
+ActiveRecord::Schema.define(version: 20161114133704) do
 
   create_table "apps", force: true do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20161104204201) do
     t.integer  "cart_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "quantity",   default: 1
   end
 
   add_index "line_items", ["cart_id"], name: "index_line_items_on_cart_id"
